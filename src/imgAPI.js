f465fff339673fb704e96c16d68b526b72f1c723
Natasha <natkamonte1992@gmail.com>
2022-11-28 22:53:13 +0200

    .

--- src/imgAPI.js
+++ src/imgAPI.js
@@ -1,22 +1,31 @@
 import Notiflix from 'notiflix';
 
+export default class imagesAPI {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
 
-const BASE_URL = 'https://pixabay.com/api/';
-const KEY = '31663443-8f4004a5a69c11dc368053c6d';
+  get query() {
+    return this.searchQuery;
+  }
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
 
-export async function imgAPI(name) {
-  try {
-    const response = await fetch(
-      `${BASE_URL}?key=${KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`
-    );
-    if (!response.ok) {
-      throw new Error(response.statusText);
+  async fetchImg(name, page) {
+    try {
+      const response = await fetch(
+        `${BASE_URL}?key=${KEY}&q=${this.name}&image_type=photo&orientation=horizontal&safesearch=true`
+      );
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return await response.json();
+    } catch (error) {
+      return Notiflix.Notify.failure(
+        '"Sorry, there are no images matching your search query. Please try again."'
+      );
     }
-    return await response.json();
-  } catch (error) {
-    return Notiflix.Notify.failure(
-      '"Sorry, there are no images matching your search query. Please try again."'
-    );
   }
 }
-
--- src/index.js
+++ src/index.js
@@ -1,68 +1,29 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
 // import debounce from 'lodash.debounce';
-import imgAPI from './imgAPI';
+import imagesAPI from './imgAPI';
 
 const form = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
-
+const imgAPI = new imagesAPI;
 
 form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(event) {
   event.preventDefault();
-  const searchQuery = event.currentTarget.elements.searchQuery.value.trim();
-  if (searchQuery === '') {
-    return;
-  };
 
-  imgAPI(searchQuery).then(data => 
-    gallery.insertAdjacentHTML = markUp(data)
-  )
+  imgAPI.searchQuery = event.currentTarget.elements.searchQuery.value.trim();
+  if (imgAPI.searchQuery === '') {
+    return;
+  }
+
+  imgAPI.fetchImg().then(data => {
+    return gallery.insertAdjacentHTML = markUp(data);
+  });
 }
 
-
-
-
-
-
-
-
-// const DEBOUNCE_DELAY = 300;
-// const input = document.querySelector('#search-box');
-// const list = document.querySelector('.country-list');
-// const div = document.querySelector('.country-info');
-
-// input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
-
-// function onSearch(event) {
-//   const searchingCountry = event.target.value.trim();
-
-//   if (searchingCountry === '') {
-//     return;
-//   };
-//   fetchCountries(searchingCountry)
-//     .then(data => {
-//       if (data.length >= 10) {
-//         Notiflix.Notify.info(
-//           'Too many matches found. Please enter a more specific name.'
-//         );
-//       } else if (data.length === 1) {
-//         list.innerHTML = '';
-//         div.innerHTML = cardMarkUp(data);
-//       } else {
-//         div.innerHTML = '';
-//         list.innerHTML = listMarkUp(data);
-//       }
-//       return data;
-//     })
-//     .catch(error =>
-//       Notiflix.Notify.failure('"Oops, there is no country with that name"')
-//     );
-// }
 function markUp(array) {
-  return array
-    .map(
+  return array.map(
       ({
         webformatURL,
         largeImageURL,
@@ -93,3 +54,32 @@ function markUp(array) {
     .join('');
 }
 
+class imagesAPI {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+
+  async fetchImg(name, page) {
+    try {
+      const response = await fetch(
+        `${BASE_URL}?key=${KEY}&q=${this.name}&image_type=photo&orientation=horizontal&safesearch=true`
+      );
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return await response.json();
+    } catch (error) {
+      return Notiflix.Notify.failure(
+        '"Sorry, there are no images matching your search query. Please try again."'
+      );
+    }
+  }
+}
