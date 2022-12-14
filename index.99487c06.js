dae3a0722f7f280799f1a55ff3c2db5d02f20b5b
NatashaChupryna <natkamonte1992@gmail.com>
2022-12-14 21:14:45 +0000

    Deploying to gh-pages from @ NatashaChupryna/goit-js-hw-11@59f12e8feaa509f7a3fda3fb955724359b953e55 🚀

---
+++ index.99487c06.js
@@ -0,0 +1,2 @@
+!function(){var d=document.querySelector(".mask");window.addEventListener("load",(function(){d.classList.add("hide")}))}();
+//# sourceMappingURL=index.99487c06.js.map
---
+++ index.a17899b3.js
@@ -0,0 +1,2 @@
+const d=document.querySelector(".mask");window.addEventListener("load",(()=>{d.classList.add("hide")}));
+//# sourceMappingURL=index.a17899b3.js.map
